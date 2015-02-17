3cd4c607ea9d7e6a5319000fb35190cf716d3410
Christian Leibig <christian.leibig@g-node.org>
2015-02-17 15:59:59 +0100

    status embedded cICA sorting 23kHz 2015-02-16

--- HDCMOSMEAsort.m
+++ HDCMOSMEAsort.m
@@ -115,8 +115,8 @@ params.ica.renorm = false; %if true renormalize W and S such that only noise
                        
 %%%%% convolutive ICA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 params.do_cICA = false;
-params.L = 5;
-params.M = 6;
+params.L = 9;
+params.M = 5;
 params.allframes_cica = 1;
 params.min_corr = 0.02;
 params.max_cluster_size = 4;
